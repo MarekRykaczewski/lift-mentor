8aa6f6b32bad0ccace715bd5322e2dfcda478684
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-11-02 19:34:17 +0100

    move Navbar to layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
-import Navbar from "./components/Navbar";
-
 export default function Home() {
   return (
-    <main className="flex bg-gray-100 min-h-screen flex-row justify-between">
-      <Navbar />
-    </main>
+    <main className="flex bg-gray-100 min-h-screen flex-col justify-between"></main>
   );
 }
