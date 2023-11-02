2cf06eb5390e114799a0ecb001856ed58a92d2d3
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-11-02 19:27:19 +0100

    add Navbar icons

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,26 +1,34 @@
 import Link from "next/link";
 import React from "react";
+import SettingsIcon from "../icons/SettingsIcon";
+import DashboardIcon from "../icons/DashboardIcon";
+import ReportsIcon from "../icons/ReportsIcon";
+import GoalsIcon from "../icons/GoalsIcon";
 
 const data = [
   {
     id: 1,
     title: "Dashboard",
     link: "/",
+    icon: <DashboardIcon />,
   },
   {
     id: 2,
     title: "Reports",
     link: "/reports",
+    icon: <ReportsIcon />,
   },
   {
     id: 3,
     title: "Goals",
     link: "/goals",
+    icon: <GoalsIcon />,
   },
   {
     id: 4,
     title: "Settings",
     link: "/settings",
+    icon: <SettingsIcon />,
   },
 ];
 
@@ -35,10 +43,11 @@ const Navbar = () => {
       <div className="flex flex-col">
         {data.map((item) => (
           <Link
-            className="py-2 px-4 mb-2 text-base md:text-lg text-indigo-600 hover:text-indigo-800 hover:bg-indigo-100 rounded-md transition duration-300"
+            className="flex items-center gap-4 py-2 px-4 mb-2 text-base md:text-lg text-indigo-600 hover:text-indigo-800 hover:bg-indigo-100 rounded-md transition duration-300"
             href={item.link}
             key={item.id}
           >
+            {item.icon}
             {item.title}
           </Link>
         ))}
