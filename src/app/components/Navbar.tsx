2e7b8d07b3d480f6f91c4bdd68e7cc20382836b3
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-11-03 13:24:50 +0100

    add profile card to Navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -5,6 +5,7 @@ import DashboardIcon from "../icons/DashboardIcon";
 import ReportsIcon from "../icons/ReportsIcon";
 import GoalsIcon from "../icons/GoalsIcon";
 import LogoIcon from "../icons/LogoIcon";
+import Image from "next/image";
 
 const data = [
   {
@@ -35,11 +36,43 @@ const data = [
 
 const Navbar = () => {
   return (
-    <div className="flex w-[500px] flex-col p-4 md:p-10 bg-white">
+    <div className="flex w-[500px] flex-col gap-4 p-4 md:p-10 bg-white">
       {/* LOGO CONTAINER */}
-      <div className="flex items-center justify-center gap-2 uppercase text-center text-2xl md:text-3xl text-indigo-500 font-bold mb-4 md:mb-8">
+      <div className="flex items-center justify-center gap-2 uppercase text-center text-2xl md:text-3xl text-indigo-500 font-bold">
         <LogoIcon />
         <span>Lift Mentor</span>
+      </div>
+      {/* PROFILE CARD CONTAINER */}
+      <div className="bg-indigo-50 p-4 rounded-lg text-gray-700">
+        <div className="flex justify-around items-center mb-4 text-center">
+          <Image
+            className="rounded-full"
+            src={"/avatar_placeholder.png"}
+            alt="Your profile picture"
+            width={60}
+            height={60}
+          />
+          <div className="flex flex-col">
+            <span className="text-xl text-center font-bold">John Doe</span>
+            <span className="text-sm text-indigo-600 text-center">
+              Building muscle
+            </span>
+          </div>
+        </div>
+        <div className="w-full flex justify-around text-center">
+          <div className="flex flex-col">
+            <span className="text-sm text-indigo-600">Age</span>
+            <span className="font-bold">25</span>
+          </div>
+          <div className="flex flex-col">
+            <span className="text-sm text-indigo-600">Height</span>
+            <span className="font-bold">180cm</span>
+          </div>
+          <div className="flex flex-col">
+            <span className="text-sm text-indigo-600">Weight</span>
+            <span className="font-bold">75kg</span>
+          </div>
+        </div>
       </div>
       {/* LINKS CONTAINER */}
       <div className="flex flex-col">
