36dcb87737e5375b363d4dedbeb946a68e468c63
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-11-11 20:04:10 +0100

    change styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="flex min-h-screen">
+        <div className="flex min-h-screen bg-mainDark">
           <Navbar />
           {children}
         </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,35 +4,37 @@ export default function Home() {
   const currentDate = new Date().toDateString();
 
   return (
-    <main className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 auto-rows-min gap-4 p-4 sm:p-6 md:p-10 w-full bg-mainDark">
-      <div className="col-span-full justify-start row-span-1 flex gap-2">
-        <div className="text-xl px-2 text-white rounded-lg font-bold">
-          Welcome back John 👋
+    <main className="w-full">
+      <div className="p-4 sm:p-6 md:p-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 auto-rows-min gap-4 ">
+        <div className="col-span-full justify-start row-span-1 flex gap-2">
+          <div className="text-xl px-2 text-white rounded-lg font-bold">
+            Welcome back John 👋
+          </div>
+          <div className="flex items-center px-2 bg-indigo-700 text-white rounded-lg font-bold">
+            {currentDate}
+          </div>
         </div>
-        <div className="flex items-center px-2 bg-indigo-700 text-white rounded-lg font-bold">
-          {currentDate}
+        <div className="flex w-full h-full col-span-full ">
+          <WeeklyStatsCard />
         </div>
-      </div>
-      <div className="flex w-full h-full col-span-full ">
-        <WeeklyStatsCard />
-      </div>
-      <div className="flex items-center justify-center p-2 border-2 rounded-lg border-gray-600 text-white font-bold drop-shadow">
-        Placeholder
-      </div>
-      <div className="flex items-center justify-center p-2 border-2 rounded-lg border-gray-600 text-white font-bold drop-shadow">
-        Placeholder
-      </div>
-      <div className="flex items-center justify-center p-2 border-2 rounded-lg border-gray-600 text-white font-bold drop-shadow">
-        Placeholder
-      </div>
-      <div className="flex items-center justify-center p-2 border-2 rounded-lg border-gray-600 text-white font-bold drop-shadow">
-        Placeholder
-      </div>
-      <div className="flex items-center justify-center p-2 border-2 rounded-lg border-gray-600 text-white font-bold drop-shadow">
-        Placeholder
-      </div>
-      <div className="flex items-center justify-center p-2 border-2 rounded-lg border-gray-600 text-white font-bold drop-shadow">
-        Placeholder
+        <div className="flex items-center justify-center p-2 border-2 rounded-lg border-gray-600 text-white font-bold drop-shadow">
+          Placeholder
+        </div>
+        <div className="flex items-center justify-center p-2 border-2 rounded-lg border-gray-600 text-white font-bold drop-shadow">
+          Placeholder
+        </div>
+        <div className="flex items-center justify-center p-2 border-2 rounded-lg border-gray-600 text-white font-bold drop-shadow">
+          Placeholder
+        </div>
+        <div className="flex items-center justify-center p-2 border-2 rounded-lg border-gray-600 text-white font-bold drop-shadow">
+          Placeholder
+        </div>
+        <div className="flex items-center justify-center p-2 border-2 rounded-lg border-gray-600 text-white font-bold drop-shadow">
+          Placeholder
+        </div>
+        <div className="flex items-center justify-center p-2 border-2 rounded-lg border-gray-600 text-white font-bold drop-shadow">
+          Placeholder
+        </div>
       </div>
     </main>
   );
